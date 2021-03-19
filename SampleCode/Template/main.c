dc44d2a38d39e74e58c49f0fcb58c7b2a600fa73
released <released@users.noreply.github.com>
2021-03-19 13:04:57 +0800

    Initial commit

---
+++ SampleCode/Template/main.c
@@ -0,0 +1,367 @@
+/*_____ I N C L U D E S ____________________________________________________*/
+#include <stdio.h>
+#include <string.h>
+#include "NuMicro.h"
+
+#include	"project_config.h"
+
+
+/*_____ D E C L A R A T I O N S ____________________________________________*/
+
+
+/*_____ D E F I N I T I O N S ______________________________________________*/
+volatile uint32_t BitFlag = 0;
+uint32_t counter_tick = 0;
+
+/*_____ M A C R O S ________________________________________________________*/
+#define ICE_LOCK_BIT
+#define SECURITY_LOCK_CONTROL
+
+
+/*_____ F U N C T I O N S __________________________________________________*/
+
+void tick_counter(void)
+{
+	counter_tick++;
+}
+
+uint32_t get_tick(void)
+{
+	return (counter_tick);
+}
+
+void set_tick(uint32_t t)
+{
+	counter_tick = t;
+}
+
+void compare_buffer(uint8_t *src, uint8_t *des, int nBytes)
+{
+    uint16_t i = 0;	
+	
+    for (i = 0; i < nBytes; i++)
+    {
+        if (src[i] != des[i])
+        {
+            printf("error idx : %4d : 0x%2X , 0x%2X\r\n", i , src[i],des[i]);
+			set_flag(flag_error , ENABLE);
+        }
+    }
+
+	if (!is_flag_set(flag_error))
+	{
+    	printf("%s finish \r\n" , __FUNCTION__);	
+		set_flag(flag_error , DISABLE);
+	}
+
+}
+
+void reset_buffer(void *dest, unsigned int val, unsigned int size)
+{
+    uint8_t *pu8Dest;
+//    unsigned int i;
+    
+    pu8Dest = (uint8_t *)dest;
+
+	#if 1
+	while (size-- > 0)
+		*pu8Dest++ = val;
+	#else
+	memset(pu8Dest, val, size * (sizeof(pu8Dest[0]) ));
+	#endif
+	
+}
+
+void copy_buffer(void *dest, void *src, unsigned int size)
+{
+    uint8_t *pu8Src, *pu8Dest;
+    unsigned int i;
+    
+    pu8Dest = (uint8_t *)dest;
+    pu8Src  = (uint8_t *)src;
+
+
+	#if 0
+	  while (size--)
+	    *pu8Dest++ = *pu8Src++;
+	#else
+    for (i = 0; i < size; i++)
+        pu8Dest[i] = pu8Src[i];
+	#endif
+}
+
+void dump_buffer(uint8_t *pucBuff, int nBytes)
+{
+    uint16_t i = 0;
+    
+    printf("dump_buffer : %2d\r\n" , nBytes);    
+    for (i = 0 ; i < nBytes ; i++)
+    {
+        printf("0x%2X," , pucBuff[i]);
+        if ((i+1)%8 ==0)
+        {
+            printf("\r\n");
+        }            
+    }
+    printf("\r\n\r\n");
+}
+
+void  dump_buffer_hex(uint8_t *pucBuff, int nBytes)
+{
+    int     nIdx, i;
+
+    nIdx = 0;
+    while (nBytes > 0)
+    {
+        printf("0x%04X  ", nIdx);
+        for (i = 0; i < 16; i++)
+            printf("%02X ", pucBuff[nIdx + i]);
+        printf("  ");
+        for (i = 0; i < 16; i++)
+        {
+            if ((pucBuff[nIdx + i] >= 0x20) && (pucBuff[nIdx + i] < 127))
+                printf("%c", pucBuff[nIdx + i]);
+            else
+                printf(".");
+            nBytes--;
+        }
+        nIdx += 16;
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void CONFIG0_Init(void)
+{
+    uint32_t au32Config[2];
+
+	uint16_t ICELOCK = BIT11;
+	uint16_t LOCK = BIT1;
+	
+    /* Enable FMC ISP function */
+    SYS_UnlockReg();	
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC_Open();
+
+    FMC_ReadConfig(au32Config, 2);
+
+	#if defined (ICE_LOCK_BIT)
+    if (au32Config[0] & ICELOCK)
+    {
+        printf("%s : ICELOCK\r\n",__FUNCTION__);
+		while(!UART_IS_TX_EMPTY(UART0));
+		
+        FMC_ENABLE_CFG_UPDATE();       		/* Enable User Configuration update. */
+        au32Config[0] &= ~ICELOCK; 
+        FMC_WriteConfig(au32Config, 2);			/* Update User Configuration CONFIG0 and CONFIG1. */
+
+        SYS->IPRST0 = SYS_IPRST0_CHIPRST_Msk;    /* Perform chip reset to make new User Config take effect. */
+    }
+	#endif
+
+	#if defined (SECURITY_LOCK_CONTROL)
+    if (au32Config[0] & LOCK)
+    {
+        printf("%s : LOCK\r\n",__FUNCTION__);		
+		while(!UART_IS_TX_EMPTY(UART0));
+		
+        FMC_ENABLE_CFG_UPDATE();       		/* Enable User Configuration update. */
+        au32Config[0] &= ~LOCK; 
+        FMC_WriteConfig(au32Config, 2);			/* Update User Configuration CONFIG0 and CONFIG1. */
+
+        SYS->IPRST0 = SYS_IPRST0_CHIPRST_Msk;    /* Perform chip reset to make new User Config take effect. */
+    }
+	#endif
+
+    FMC_Close();                       			/* Disable FMC ISP function */
+    SYS_LockReg();	
+}	
+
+void TMR1_IRQHandler(void)
+{
+	static uint32_t LOG = 0;
+
+	
+    if(TIMER_GetIntFlag(TIMER1) == 1)
+    {
+        TIMER_ClearIntFlag(TIMER1);
+		tick_counter();
+
+		if ((get_tick() % 1000) == 0)
+		{
+        	printf("%s : %4d\r\n",__FUNCTION__,LOG++);
+			PH0 ^= 1;
+		}
+
+		if ((get_tick() % 50) == 0)
+		{
+
+		}	
+    }
+}
+
+
+void TIMER1_Init(void)
+{
+    TIMER_Open(TIMER1, TIMER_PERIODIC_MODE, 1000);
+    TIMER_EnableInt(TIMER1);
+    NVIC_EnableIRQ(TMR1_IRQn);	
+    TIMER_Start(TIMER1);
+}
+
+void UARTx_Process(void)
+{
+	uint8_t res = 0;
+	res = UART_READ(UART0);
+
+	if (res == 'x' || res == 'X')
+	{
+		NVIC_SystemReset();
+	}
+
+	if (res > 0x7F)
+	{
+		printf("invalid command\r\n");
+	}
+	else
+	{
+		switch(res)
+		{
+			case '1':
+				break;
+
+			case 'X':
+			case 'x':
+			case 'Z':
+			case 'z':
+				NVIC_SystemReset();		
+				break;
+		}
+	}
+}
+
+
+void UART0_IRQHandler(void)
+{
+    if(UART_GET_INT_FLAG(UART0, UART_INTSTS_RDAINT_Msk | UART_INTSTS_RXTOINT_Msk))     /* UART receive data available flag */
+    {
+        while(UART_GET_RX_EMPTY(UART0) == 0)
+        {
+			UARTx_Process();
+        }
+    }
+
+    if(UART0->FIFOSTS & (UART_FIFOSTS_BIF_Msk | UART_FIFOSTS_FEF_Msk | UART_FIFOSTS_PEF_Msk | UART_FIFOSTS_RXOVIF_Msk))
+    {
+        UART_ClearIntFlag(UART0, (UART_INTSTS_RLSINT_Msk| UART_INTSTS_BUFERRINT_Msk));
+    }	
+}
+
+void UART0_Init(void)
+{
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+
+	/* Set UART receive time-out */
+	UART_SetTimeoutCnt(UART0, 20);
+
+	UART0->FIFO &= ~UART_FIFO_RFITL_4BYTES;
+	UART0->FIFO |= UART_FIFO_RFITL_8BYTES;
+
+	/* Enable UART Interrupt - */
+	UART_ENABLE_INT(UART0, UART_INTEN_RDAIEN_Msk | UART_INTEN_TOCNTEN_Msk | UART_INTEN_RXTOIEN_Msk);
+	
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	#if (_debug_log_UART_ == 1)	//debug
+	printf("\r\nCLK_GetCPUFreq : %8d\r\n",CLK_GetCPUFreq());
+	printf("CLK_GetHXTFreq : %8d\r\n",CLK_GetHXTFreq());
+	printf("CLK_GetLXTFreq : %8d\r\n",CLK_GetLXTFreq());	
+	printf("CLK_GetPCLK0Freq : %8d\r\n",CLK_GetPCLK0Freq());
+	printf("CLK_GetPCLK1Freq : %8d\r\n",CLK_GetPCLK1Freq());	
+	#endif
+}
+
+void Custom_Init(void)
+{	
+	//EVM LED
+	GPIO_SetMode(PH,BIT0,GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PH,BIT1,GPIO_MODE_OUTPUT);
+	GPIO_SetMode(PH,BIT2,GPIO_MODE_OUTPUT);
+	
+}
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk|CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk|CLK_STATUS_HIRCSTB_Msk);
+
+    CLK_EnableXtalRC(CLK_PWRCTL_LXTEN_Msk|CLK_PWRCTL_LIRCEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_LXTSTB_Msk|CLK_STATUS_LIRCSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_192MHZ);
+    /* Set PCLK0/PCLK1 to HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Enable UART clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    /* Select UART clock source from HXT */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk);
+    SYS->GPB_MFPH |= (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    CLK_EnableModuleClock(TMR1_MODULE);
+    CLK_SetModuleClock(TMR1_MODULE, CLK_CLKSEL1_TMR1SEL_HIRC, 0);
+	
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*
+ * This is a template project for M480 series MCU. Users could based on this project to create their
+ * own application without worry about the IAR/Keil project settings.
+ *
+ * This template application uses external crystal as HCLK source and configures UART0 to print out
+ * "Hello World", users may need to do extra system configuration based on their system design.
+ */
+
+int main()
+{
+    SYS_Init();
+
+	UART0_Init();
+	Custom_Init();	
+	TIMER1_Init();
+
+	CONFIG0_Init();
+
+
+    /* Got no where to go, just loop forever */
+    while(1)
+    {
+
+
+    }
+}
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
